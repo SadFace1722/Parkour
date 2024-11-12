a960297e0fac40675c92a930b44c3dfa2eee2733
NhutBe <126226871+NhutBe@users.noreply.github.com>
2024-11-12 23:24:21 +0700

    Nhut Minigame Map 3

---
+++ Assets/Scripts/Stuff/Map3/CubeRed.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRed : MonoBehaviour, PlayerInterface
+{
+    public static bool isRedPressed = false; // Biến kiểm tra xem người chơi có nhấn đúng Red hay không
+    Animator animator;
+    private void Start()
+    {
+        animator = transform.parent.GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        AnimaButton();
+    }
+    public void Interact()
+    {
+        // Khi người chơi nhấn vào cube đỏ
+        if (GameLaser.Instance.CheckOrder("Red"))
+        {
+            isRedPressed = true;
+            Debug.Log("Red Cube pressed correctly!");
+        }
+        else
+        {
+            GameLaser.Instance.ResetOrder();
+            Debug.Log("Wrong order! Resetting.");
+        }
+    }
+    public void AnimaButton()
+    {
+        if (isRedPressed == true)
+        {
+            animator.SetBool("button", true);
+        }
+        else if (GameLaser.Instance.currentStep==0)
+        {
+            animator.SetBool("button", false);
+        }
+    }                   
+}
