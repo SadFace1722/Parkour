14a797dcef77f24e0e568fe29a1ac178867be793
thhhuy <phuoctuan126@gmail.com>
2024-11-20 17:24:08 +0700

    sua lai phan keypad

--- Assets/Scripts/Stuff/Map3/PadPass.cs
+++ Assets/Scripts/Stuff/Map3/PadPass.cs
@@ -4,12 +4,38 @@ using UnityEngine;
 public class PadInteraction : MonoBehaviour, PlayerInterface
 {
     [SerializeField] private GameObject PassWordCanvas; // Canvas nhập mật khẩu
-    bool isActive;
+    [SerializeField] private CharacterController playerController;
+    private bool isActive;
+
     public void Interact()
     {
+   
         isActive = !isActive;
 
+        if (isActive)
+        {
+            if (playerController != null)
+            {
+                playerController.enabled = false;
+            }
+
+           
+            Cursor.lockState = CursorLockMode.Confined; 
+            Cursor.visible = true; 
+        }
+        else
+        {
+            if (playerController != null)
+            {
+                playerController.enabled = true;
+            }
+
+            
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = false; 
+        }
     }
+
     void Update()
     {
         PassWordCanvas.SetActive(isActive);
