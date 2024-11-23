714e2bf0a8afc3a97ee494b62ad91d839ef2fd43
oktbmal <quocgame3@gmail.com>
2024-11-23 13:49:22 +0700

    run

--- Assets/Scripts/Boss/PatrolState.cs
+++ Assets/Scripts/Boss/PatrolState.cs
@@ -8,10 +8,15 @@ public class PatrolState : StateMachineBehaviour
     List<Transform> wayPoints = new List<Transform>();
     NavMeshAgent agent;
 
+    Transform player;
+    float chaseRange = 8;
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+       player = GameObject.FindGameObjectWithTag("Player").transform;
        agent = animator.GetComponent<NavMeshAgent>();
+       agent.speed = 1.5f;
        timer = 0;
        GameObject go = GameObject.FindGameObjectWithTag("WayPoints");
        foreach(Transform t in go.transform)
@@ -29,6 +34,9 @@ public class PatrolState : StateMachineBehaviour
        timer += Time.deltaTime;
        if (timer > 10 )
             animator.SetBool("isPatrolling", false);
+      float distance = Vector3.Distance(player.position, animator.transform.position);
+       if (distance < chaseRange)
+            animator.SetBool("isChasing", true);
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
