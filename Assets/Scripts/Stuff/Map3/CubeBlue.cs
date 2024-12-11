7c51d1480e445d32be1c488865c92753f86d0c46
NhutBe <126226871+NhutBe@users.noreply.github.com>
2024-12-12 03:38:59 +0700

    nhut sua nut va check code

--- Assets/Scripts/Stuff/Map3/CubeBlue.cs
+++ Assets/Scripts/Stuff/Map3/CubeBlue.cs
@@ -4,22 +4,12 @@ using UnityEngine;
 
 public class CubeBlue : MonoBehaviour, PlayerInterface
 {
-    public static bool isBluePressed = false; // Biến kiểm tra xem người chơi có nhấn đúng Blue hay không
     Animator animator;
 
     private void Start()
     {
         animator = transform.parent.GetComponent<Animator>();
-        animator.SetBool("button", false); // Đảm bảo bắt đầu với trạng thái idle
-    }
-
-    private void Update()
-    {
-        // Animation thụt vào khi nút bị nhấn
-        if (isBluePressed)
-        {
-            animator.SetBool("button", true); // Thực hiện animation
-        }
+        animator.SetBool("button", false); // Đảm bảo bắt đầu ở trạng thái idle
     }
 
     public void Interact()
@@ -30,29 +20,17 @@ public class CubeBlue : MonoBehaviour, PlayerInterface
             return;
         }
 
-        // Khi người chơi nhấn vào cube xanh dương
-        if (GameLaser.Instance.CheckOrder("Blue") && !isBluePressed) // Chỉ khi nhấn đúng và chưa nhấn
+        // Thay đổi trạng thái animation khi người chơi nhấn
+        if (animator.GetBool("button"))
         {
-            isBluePressed = true;
-            Debug.Log("Blue Cube pressed correctly!");
+            animator.SetBool("button", false); // Quay lại trạng thái idle ngay lập tức
         }
         else
         {
-            GameLaser.Instance.ResetOrder();
-            Debug.Log("Wrong order! Resetting.");
-            DisableButton(); // Vô hiệu hóa nút sau khi sai thứ tự
+            animator.SetBool("button", true); // Chuyển sang trạng thái thụt vào
         }
 
-        // Dù đúng hay sai thứ tự, animation thụt vào vẫn phải xảy ra khi ấn
-        if (!isBluePressed)
-        {
-            animator.SetBool("button", true);
-        }
-    }
-
-    // Vô hiệu hóa collider sau khi sai thứ tự
-    private void DisableButton()
-    {
-        GetComponent<Collider>().enabled = false; // Vô hiệu hóa collider của CubeBlue
+        // Kiểm tra thứ tự của Blue
+        GameLaser.Instance.CheckOrder("Blue");
     }
 }
--- Assets/Scripts/Stuff/Map3/CubeYellow.cs
+++ Assets/Scripts/Stuff/Map3/CubeYellow.cs
@@ -4,22 +4,12 @@ using UnityEngine;
 
 public class CubeYellow : MonoBehaviour, PlayerInterface
 {
-    public static bool isYellowPressed = false; // Biến kiểm tra xem người chơi có nhấn đúng Yellow hay không
     Animator animator;
 
     private void Start()
     {
         animator = transform.parent.GetComponent<Animator>();
-        animator.SetBool("button", false); // Đảm bảo bắt đầu với trạng thái idle
-    }
-
-    private void Update()
-    {
-        // Animation thụt vào khi nút bị nhấn
-        if (isYellowPressed)
-        {
-            animator.SetBool("button", true); // Thực hiện animation
-        }
+        animator.SetBool("button", false); // Đảm bảo bắt đầu ở trạng thái idle
     }
 
     public void Interact()
@@ -30,23 +20,17 @@ public class CubeYellow : MonoBehaviour, PlayerInterface
             return;
         }
 
-        // Khi người chơi nhấn vào cube vàng
-        if (GameLaser.Instance.CheckOrder("Yellow") && !isYellowPressed) // Chỉ khi nhấn đúng và chưa nhấn
+        // Thay đổi trạng thái animation khi người chơi nhấn
+        if (animator.GetBool("button"))
         {
-            isYellowPressed = true;
-            Debug.Log("Yellow Cube pressed correctly!");
+            animator.SetBool("button", false); // Quay lại trạng thái idle ngay lập tức
         }
         else
         {
-            GameLaser.Instance.ResetOrder();
-            Debug.Log("Wrong order! Resetting.");
-            DisableButton(); // Vô hiệu hóa nút sau khi sai thứ tự
+            animator.SetBool("button", true); // Chuyển sang trạng thái thụt vào
         }
-    }
 
-    // Vô hiệu hóa collider sau khi sai thứ tự
-    private void DisableButton()
-    {
-        GetComponent<Collider>().enabled = false; // Vô hiệu hóa collider của CubeYellow
+        // Kiểm tra thứ tự của Yellow
+        GameLaser.Instance.CheckOrder("Yellow");
     }
 }
